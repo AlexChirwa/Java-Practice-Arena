1c1de09b63409d52ba2ab133fa14df5af4b393fd
Alex Joshua Chirwa <153588378+AlexChirwa@users.noreply.github.com>
2024-05-30 17:55:04 +0800

    Add files via upload

---
+++ FlowControl6.java
@@ -0,0 +1,20 @@
+// Date: 5.29.24
+
+/* Question 3 While loop:
+ * Write a Java program to print numbers from 1 to 10 using a while loop.
+ * 
+ * Expected Output:
+ * 1 2 3 4 5 6 7 8 9 10
+ */
+
+
+public class FlowControl6{
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while( i <= 10){
+			System.out.println(i);
+			i++;
+		}
+	}
+}
